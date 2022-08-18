ba0c448bbfd686be3a9cf39a6fd98b0f957cd324
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-18 17:34:43 +0300

    Добавлена разметка галереи

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const linkGallery = document.querySelector('.gallery');
+console.log(linkGallery);
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+linkGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+//linkGallery.addEventListener('click', makeClickImg);
+
+//разметка галереи
+function makeGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+              <a class="gallery__link" href="large-image.jpg">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+             </a>
+           </div>`;
+    })
+    .join('');
+}
