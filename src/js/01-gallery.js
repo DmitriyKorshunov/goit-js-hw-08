b1acd76d26adc2703aa85659feb69eab9175f729
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-18 21:02:30 +0300

    Галерея картинок

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,26 @@
 // Add imports above this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
 const linkGallery = document.querySelector('.gallery');
-console.log(linkGallery);
-const galleryMarkup = makeGalleryMarkup(galleryItems);
 
-linkGallery.insertAdjacentHTML('beforeend', galleryMarkup);
-//linkGallery.addEventListener('click', makeClickImg);
-
-//разметка галереи
-function makeGalleryMarkup() {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-              <a class="gallery__link" href="large-image.jpg">
-                <img
-                  class="gallery__image"
-                  src="${preview}"
-                  data-source="${original}"
-                  alt="${description}"
-                />
-             </a>
-           </div>`;
-    })
-    .join('');
+function makeGalleryItem(galleryItem) {
+  const { preview, original, description } = galleryItem;
+  return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
 }
+
+const makeGalleryItems = galleryItems.map(makeGalleryItem).join('');
+
+linkGallery.insertAdjacentHTML('beforeend', makeGalleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
