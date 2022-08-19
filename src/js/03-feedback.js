861129c2d21d91f958aea7d7c2c76dd816be7268
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-19 22:05:35 +0300

    Зад 3 форма

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+form.addEventListener('submit', throttle(onFormSubmit, 500));
+form.addEventListener('input', onValueInput);
+
+populateMessageTextareal();
+
+function onValueInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formDataParse = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const formEmail = formDataParse.email;
+  const formMsg = formDataParse.message;
+  console.log(`'Email:', ${formEmail}, 'Message:', ${formMsg}`);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateMessageTextareal() {
+  const valueStorage = localStorage.getItem(STORAGE_KEY);
+  if (valueStorage) {
+    const formDataParse = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    form.elements.email.value = formDataParse.email;
+    form.elements.message.value = formDataParse.message;
+  }
+}
