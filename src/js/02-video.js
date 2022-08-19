230973a983b8a2e190e521aeed1d3e1efa0503e9
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-19 13:22:23 +0300

    Зад 2 видеоплеер

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const VIDEO_WATCHING_TIME = 'videoplayer-current-time';
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(VIDEO_WATCHING_TIME, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(VIDEO_WATCHING_TIME);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
